a34e41713be1cfd55fb03e51e1242bcd3d214fed
!cake <cakenotfound@googlemail.com>
2013-04-07 11:10:33 +0000

    [GLib] Added debug message logging to EndPoint.

--- lua/glib/protocol/endpoint.lua
+++ lua/glib/protocol/endpoint.lua
@@ -22,6 +22,8 @@ function self:ctor (remoteId, systemName)
 			self:ProcessSessions ()
 		end
 	)
+	
+	self.Log = {}
 end
 
 function self:dtor ()
@@ -37,12 +39,20 @@ function self:CloseSession (session)
 	if not self.Sessions [session:GetId ()] then return end
 	
 	if not session:HasQueuedPackets () then
-		GLib.Debug (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ": Session " .. session:ToString () .. " closed.")
+		self:DebugMessage (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ": Session " .. session:ToString () .. " closed.")
 		session:DispatchEvent ("Closed")
 		self.Sessions [session:GetId ()] = nil
 	end
 	
 	session:Close ()
+end
+
+function self:DebugMessage (message)
+	message = tostring (CurTime ()) .. ": " .. message
+	self.Log [#self.Log + 1] = message
+	if #self.Log > 100 then
+		table.remove (self.Log, 1)
+	end
 end
 
 function self:GetRemoteId ()
@@ -74,10 +84,10 @@ function self:ProcessSessions ()
 	local closed = {}
 	for _, session in pairs (self.Sessions) do
 		session:Tick ()
-	
+		
 		local outBuffer = session:DequeuePacket ()
 		if outBuffer then
-			GLib.Debug (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ": Session " .. session:ToString () .. " packet sent to " .. self.RemoteId .. ".")
+			self:DebugMessage (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ": Session " .. session:ToString () .. " packet sent to " .. self.RemoteId .. ".")
 			GLib.Net.DispatchPacket (self.RemoteId, self.DataChannel, outBuffer)
 		end
 		
@@ -92,7 +102,7 @@ function self:ProcessSessions ()
 	
 	for session, _ in pairs (timedOut) do
 		if not session:IsClosing () then
-			GLib.Debug (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ": Session " .. session:ToString () .. " timed out.")
+			self:DebugMessage (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ": Session " .. session:ToString () .. " timed out.")
 			session:DispatchEvent ("TimedOut")
 			session:HandleTimeOut ()
 			session:Close ()
@@ -106,7 +116,7 @@ end
 
 function self:SendNotification (session)
 	session:SetRemoteEndPoint (self)
-	GLib.Debug (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ":SendNotification : " .. session:ToString ())
+	self:DebugMessage (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ":SendNotification : " .. session:ToString ())
 	
 	local outBuffer = GLib.Net.OutBuffer ()
 	outBuffer:UInt32 (session:GetTypeId ())
@@ -120,7 +130,7 @@ function self:StartSession (session)
 	session:SetId (self.NextSessionId)
 	self.NextSessionId = self.NextSessionId + 1
 	
-	GLib.Debug (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ":StartSession : New " .. session:ToString ())
+	self:DebugMessage (self.SystemName .. ".Net.EndPoint." .. self.UniqueId .. ":StartSession : New " .. session:ToString ())
 	
 	self.Sessions [session:GetId ()] = session
 	
