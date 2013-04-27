496b08a6672a250fb0cd480ed6d105b84b735e05
!cake <cakenotfound@googlemail.com>
2013-04-27 23:52:28 +0000

    [GLib] Moved MemoryUsageReport and containers from GCompute to GLib.

--- lua/glib/stage2.lua
+++ lua/glib/stage2.lua
@@ -5,12 +5,23 @@ include ("colors.lua")
 
 include ("coroutine.lua")
 
+include ("memoryusagereport.lua")
+
 include ("unicode/unicodecategory.lua")
 include ("unicode/wordtype.lua")
 include ("unicode/utf8.lua")
 include ("unicode/unicode.lua")
 include ("unicode/unicodecategorytable.lua")
 include ("unicode/transliteration.lua")
+
+GLib.Containers = {}
+include ("containers/binarytree.lua")
+include ("containers/binarytreenode.lua")
+include ("containers/linkedlist.lua")
+include ("containers/linkedlistnode.lua")
+include ("containers/queue.lua")
+include ("containers/stack.lua")
+include ("containers/tree.lua")
 
 include ("net/net.lua")
 include ("net/datatype.lua")
