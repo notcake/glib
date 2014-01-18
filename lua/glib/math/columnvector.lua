4f45c4098ac3007f43d623db6983e51d2014cc4c
!cake <cakenotfound@googlemail.com>
2014-01-18 22:53:56 +0000

    [GLib] More matrix functions.

--- lua/glib/math/columnvector.lua
+++ lua/glib/math/columnvector.lua
@@ -4,3 +4,7 @@ local ctor = GLib.MakeConstructor (self, GLib.Vector)
 function GLib.ColumnVector (h, ...)
 	return ctor (1, h, ...)
 end
+
+function self:SetElementCount (elementCount)
+	self.Height = elementCount
+end
--- lua/glib/math/rowvector.lua
+++ lua/glib/math/rowvector.lua
@@ -4,3 +4,7 @@ local ctor = GLib.MakeConstructor (self, GLib.Vector)
 function GLib.RowVector (w, ...)
 	return ctor (w, 1, ...)
 end
+
+function self:SetElementCount (elementCount)
+	self.Width = elementCount
+end
