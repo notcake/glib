5a6a4255d8381c52054dcbdec8bf63298dfdb58b
notcake <cakenotfound@gmail.com>
2014-04-01 01:12:23 +0100

    [GLib] Fixed String.EscapeNonprintable not correctly escaping quotation marks.

--- lua/glib/string/escaping.lua
+++ lua/glib/string/escaping.lua
@@ -39,6 +39,7 @@ function GLib.String.EscapeNonprintable (str)
 			c = string.byte (c)
 			if c < string.byte (" ") then return string.format ("\\x%02x", c) end
 			if c >= 127 then return string.format ("\\x%02x", c) end
+			if c == string.byte ("\"") then return "\\\"" end
 		end
 	)
 	return str
